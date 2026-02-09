f28d835ffad35d52487323c7f90a59ee93b4a100
theredenemy <1234corbin.1234ad@gmail.com>
2026-02-08 20:10:46 -0800

    modified:   client.py

--- client.py
+++ client.py
@@ -2,45 +2,52 @@ import os
 import socket
 import pathlib
 import time
-ip = "10.0.0.116"
+ip = "10.0.0.51"
 port = 4456
 SIZE = 1024
 
 def SendFile(thefile, ip, port):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((ip, port))
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((ip, port))
 
-    file = open(thefile, 'rb')
-    filesize = os.path.getsize(thefile)
-    filename = pathlib.Path(thefile)
+        file = open(thefile, 'rb')
+        filesize = os.path.getsize(thefile)
+        filename = pathlib.Path(thefile)
+        mainsize = SIZE
+        if filesize >= SIZE:
+            mainsize = filesize
+        client.send(f"{filename}".encode())
+        while True:
+            data = client.recv(SIZE).decode()
+            if data == "SENT":
+                data = "wait"
+                break
+        client.send(str(mainsize).encode())
+        while True:
+            data = client.recv(SIZE).decode()
+            if data == "SENT":
+                data = "wait"
+                break
 
-    client.send(f"{filename}".encode())
-    while True:
-        data = client.recv(SIZE).decode()
-        if data == "SENT":
-            data = "wait"
-            break
-    client.send(str(filesize).encode())
-    while True:
-        data = client.recv(SIZE).decode()
-        if data == "SENT":
-            data = "wait"
-            break
-
-    data = file.read()
-    client.sendall(data)
-    while True:
-        data = client.recv(SIZE).decode()
-        if data == "SENT":
-            data = "wait"
-            break
-    client.send(b"<END>")
-    while True:
-        data = client.recv(SIZE).decode()
-        if data == "SENT":
-            data = "wait"
-            break
-    return True
+        data = file.read()
+        client.sendall(data)
+        while True:
+            data = client.recv(SIZE).decode()
+            if data == "SENT":
+                data = "wait"
+                break
+        client.send(b"<END>")
+        while True:
+            data = client.recv(SIZE).decode()
+            if data == "SENT":
+                data = "wait"
+                break
+        print(f"File Sent : {thefile}")
+        return True
+    except Exception as e:
+        print(f"Error Sending File: {e}")
+        return False
 
 if __name__ == "__main__":
-    SendFile("inputs_test.txt", ip, port)
+    SendFile("inputs.txt", ip, port)
